87a3636c13b6567cc9fa8453c57986aa9dc7cb85
github username <github email address>
2019-10-17 11:44:57 -0700

    created a functions.ts file which contains examples of different functions.

--- functions.js
+++ functions.js
@@ -1,34 +1,26 @@
-function getSum(num1:number, num2:number):number {
+function getSum(num1, num2) {
     return num1 + num2;
 }
-
 // console.log(getSum(1, 4));
-
 //accepts any type as an argument but returns a number
-let mySum = function(num1:any, num2:any):number {
-    if(typeof num1 == "string") {
+var mySum = function (num1, num2) {
+    if (typeof num1 == "string") {
         num1 = parseInt(num1);
     }
-
-    if(typeof num2 == "string") {
+    if (typeof num2 == "string") {
         num2 = parseInt(num2);
     }
-
     return num1 + num2;
-}
-
+};
 // console.log(mySum(3, 5));
-
 // ? makes the argument optional
-function getName(firstName:string, lastName?:string):string {
-    if(lastName == undefined) {
+function getName(firstName, lastName) {
+    if (lastName == undefined) {
         return firstName;
     }
     return firstName + " " + lastName;
 }
-
 // console.log(getName("Aliya", "Lewis"));
-
-function myVoid():void {
+function myVoid() {
     return;
 }
