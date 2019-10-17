cdd14dbbc8153c488cabd5ecb767d9644f9849a1
github username <github email address>
2019-10-17 10:47:59 -0700

    Learning the basics of TypeScript

---
+++ types.js
@@ -0,0 +1,24 @@
+var myString;
+var myNum;
+var myBool;
+var myVar;
+// this has to be an array of strings
+var strArr;
+// an array of numbers
+var numArr;
+// array of booleans
+var booArr;
+// strings can be concatenated
+// function return:
+// myString = 'Hello'.slice(0, 3)
+myString = "Hello World";
+// numbers can be positive or negative integers
+// expressions (5 + 5)
+// decimals and hexidecimals (0xf00d)
+myNum = 22;
+myBool = true;
+myVar = 5;
+strArr = ["Hello", "World"];
+numArr = [1, 2, 3];
+booArr = [true, false, false];
+console.log(myString, myNum);
---
+++ types.ts
@@ -0,0 +1,35 @@
+let myString: string;
+let myNum: number;
+let myBool: boolean;
+let myVar: any;
+
+// this has to be an array of strings
+let strArr: string[];
+
+// an array of numbers
+let numArr: number[];
+
+// array of booleans
+let booArr: boolean[];
+
+// strings can be concatenated
+// function return:
+// myString = 'Hello'.slice(0, 3)
+myString = "Hello World";
+
+// numbers can be positive or negative integers
+// expressions (5 + 5)
+// decimals and hexidecimals (0xf00d)
+myNum = 22;
+
+myBool = true;
+
+myVar = 5; 
+
+strArr = ["Hello", "World"];
+
+numArr = [1, 2, 3];
+
+booArr = [true, false, false];
+
+console.log(myString, myNum);
